73efc2eb80311051055a454d137e3901af201aec
Your Name <@youexample.com>
2026-03-21 08:48:06 +0900

    updated main.py and requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import flask
+from flask import Flask, render_template, request, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
 if(__name__ == '__main__'):
     app.run()
